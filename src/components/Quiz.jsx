f4a5145a314f785a824c5d49f4f9be0ed1c418c8
Matt Pahuta <mhpahuta@gmail.com>
2025-06-25 16:26:14 -0700

    Add quiz answer scoring functionality and styles

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
+import iconCorrect from "../assets/icon-correct.svg";
+import iconIncorrect from "../assets/icon-incorrect.svg";
+
+const letters = ['A', 'B', 'C', 'D'];
 
 function Quiz({quiz, question, questionIndex, totalQuestions, onAnswerSubmit}) {
 
   const [selectedOption, setSelectedOption] = useState(null);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
 
+  const isCorrect = selectedOption === question.answer;
   console.log(question.question, questionIndex, totalQuestions);
 
   function handleSelectedOption(option) {
-    setSelectedOption(option)
+    if (!hasSubmitted) setSelectedOption(option);
   }
 
   function handleSubmit() {
     if (selectedOption === null) return;
+    setHasSubmitted(true);
+  }
+  
+  function handleNext() {
     onAnswerSubmit(selectedOption);
-    setSelectedOption(null); // reset for next questions/answer
+    setSelectedOption(null);
+    setHasSubmitted(false);
   }
 
   // ToDo: use clsx library to conditionally render correct/incorrect styles
+  // - button styles for incorrect/correct states
 
   return (
-    <div className="wrapper grid-columns">
+    <section className="wrapper grid-columns">
       <div className="quiz-question-info">
         <h1 className="visually-hidden">{quiz} quiz</h1>
         <p className="accent-text">Question {questionIndex + 1} of {totalQuestions}</p>
@@ -27,25 +39,55 @@ function Quiz({quiz, question, questionIndex, totalQuestions, onAnswerSubmit}) {
         <div className="quiz-progress-bar"></div>
       </div>
       <div className="selection-container multiple-choice-answers">
-        {question.options.map((option) => (
-          <button 
-            key={option} 
-            onClick={() => handleSelectedOption(option)} 
-            className={`button option-button ${selectedOption === option ? 'selected' : ''}`}
+        {question.options.map((option, index) => {
+          const isSelected = selectedOption === option;
+          const isAnswer = option === question.answer;
+          // const isCorrect = selectedOption === question.answer;
+          // console.log(`isCorrect: ${isCorrect}`)
+          let status = '';
+          if (hasSubmitted) {
+            if (isAnswer) {
+              status = 'correct';
+            } else if (isSelected) {
+              status = 'incorrect';
+            }
+
+          }
+
+          return (
+            <button 
+              key={`${option}-${index}`} 
+              onClick={() => handleSelectedOption(option)} 
+              className={`button option-button ${isSelected ? 'selected' : ''} ${status}`}
+            >
+              <span className="option-letter">{letters[index]}</span>
+              <span className="option-text">{option}</span>
+
+              {/* correct/incorrect icon */}
+              {hasSubmitted && isSelected && !isAnswer && (
+                <img src={iconIncorrect} alt="" className="icon icon-feedback" />
+              )}
+              {hasSubmitted && isAnswer && (
+                <img src={iconCorrect} alt="" className="icon icon-feedback" />
+              )}
+            </button>
+          );
+        })}
+        
+        <div className="quiz-submit">
+          <button
+            className="button submit-option-button"
+            onClick={hasSubmitted ? handleNext : handleSubmit}
+            // ToDo: add validation instead of disabling button
+            disabled={selectedOption === null}
           >
-            <span className="option-letter">A</span>
-            {option}
+            {hasSubmitted ? "Next Question" : "Submit Answer"}
           </button>
-        ))}
-        <button
-          className="button submit-option-button"
-          onClick={handleSubmit}
-          disabled={selectedOption === null}
-        >
-          Submit Answer
-        </button>
+
+        </div>
+
       </div>
-    </div>
+    </section>
   );
 }
 
